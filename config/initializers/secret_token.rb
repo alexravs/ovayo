d034c35144de044d9f750bde0eb249fc7a29995f
Alex Ravet <ravet.alexandre@gmail.com>
2013-12-24 22:23:09 +0100

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ovayo::Application.config.secret_key_base = 'cd1c472f0d54cd8e5b75ebc30857072c1f9d40edf01314f1a9244d7df999ee87279cd1026ae92f68c1f21d78978a16d92c516165c90f5ed199037d551074c0a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ovayo::Application.config.session_store :cookie_store, key: '_Ovayo_session'
